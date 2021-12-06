3ba4cf486ffd584f8268437017ae597bb1570814
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-12-06 23:07:50 +0900

    Update: set font-family at default html

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,7 @@ html {
   color: $black;
   font-size: 10px;
   font-weight: 400;
+  font-family: $noto-sans, $open-sans, $montserrat, $noto-serif;
   word-spacing: 1px;
   -webkit-text-size-adjust: 100%;
   -ms-text-size-adjust: 100%;
