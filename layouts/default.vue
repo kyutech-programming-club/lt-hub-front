29c0e0b4167ae7ff2d7b2922bb235e655e996cdd
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-12-06 22:39:21 +0900

    Add: default vue

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <Nuxt />
+</template>
+
+<style lang="scss" scoped>
+html {
+  background: $black-lighten-4;
+  color: $black;
+  font-size: 10px;
+  font-weight: 400;
+  word-spacing: 1px;
+  -webkit-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,5 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Hello, world!</h1>
+  </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
