c9d2bbd263f9085ac27341f71f7abfdc9c7b3dc2
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-12-14 23:08:37 +0900

    Add: types

---
+++ types/index.ts
@@ -0,0 +1,68 @@
+export interface LoginUser {
+  userId: string;
+  avatarUrl: string;
+}
+
+export interface Event {
+  eventId: string;
+  title: string;
+  date: string;
+  location: string;
+  state: string;
+  participants: number;
+  hostId: string;
+  hostName: string;
+  avatarUrl: string;
+}
+
+export interface EventDetail {
+  eventId: string;
+  title: string;
+  description: string;
+  startTime: string;
+  endTime: string;
+  location: string;
+  hostId: string;
+  hostName: string;
+  avatarUrl: string;
+}
+
+export interface Participant {
+  participantId: string;
+  userId: string;
+  userName: string;
+  avatarUrl: string;
+  state: string;
+}
+
+export interface Talk {
+  talkId: string;
+  title: string;
+  userId: string;
+  userName: string;
+  avatarUrl: string;
+  state: string;
+}
+
+export interface TalkDetail {
+  talkId: string;
+  title: string;
+  eventId: string;
+  eventName: string;
+  state: string;
+  userId: string;
+  userName: string;
+  avatarUrl: string;
+}
+
+export interface User {
+  userId: string;
+  avatarUrl: string;
+  name: string;
+  githubId: string;
+  twitterId: string;
+}
+
+export interface Suggest {
+  theme: string;
+}
