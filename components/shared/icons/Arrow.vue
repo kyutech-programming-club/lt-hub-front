9241bdde07f87b854691ae31c0a25e2002444415
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-09 18:02:58 +0900

    Add: icons

---
+++ components/shared/icons/Arrow.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="10.826"
+    height="6.828"
+    viewBox="0 0 10.826 6.828"
+  >
+    <g transform="translate(1.414 1.414)">
+      <line
+        x2="4"
+        y2="4"
+        fill="none"
+        stroke="#707070"
+        stroke-linecap="round"
+        stroke-width="2"
+      />
+      <line
+        x2="3.997"
+        y2="3.997"
+        transform="translate(7.997 0) rotate(90)"
+        fill="none"
+        stroke="#707070"
+        stroke-linecap="round"
+        stroke-width="2"
+      />
+    </g>
+  </svg>
+</template>
---
+++ components/shared/icons/Menu.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="26"
+    height="6"
+    viewBox="0 0 26 6"
+  >
+    <g transform="translate(-1389 -7830)">
+      <circle
+        cx="3"
+        cy="3"
+        r="3"
+        transform="translate(1389 7830)"
+        fill="#757575"
+      />
+      <circle
+        cx="3"
+        cy="3"
+        r="3"
+        transform="translate(1399 7830)"
+        fill="#757575"
+      />
+      <circle
+        cx="3"
+        cy="3"
+        r="3"
+        transform="translate(1409 7830)"
+        fill="#757575"
+      />
+    </g>
+  </svg>
+</template>
