9f1100c4ec82a31ea12117a86b78942576f2ee67
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-11 22:22:42 +0900

    Add: base box component DOM

---
+++ components/shared/BaseBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+    <FlexContainer class="box__header">
+      <h3 class="box__title">
+        <slot name="title" />
+      </h3>
+      <span class="box__sub">
+        <slot name="sub" />
+      </span>
+    </FlexContainer>
+    <slot name="content" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+import FlexContainer from "~/components/layout/FlexContainer.vue";
+
+export default defineComponent({
+  components: {
+    FlexContainer,
+  },
+});
+</script>
