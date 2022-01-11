244bf2870187c6fe7664f899e07fa20468192fb6
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-11 22:25:20 +0900

    Add: main container

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <h1>Hello, world!</h1>
+  <div class="main">
+    <Container>
+      <h1>Hello, world!</h1>
+    </Container>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+import Container from "~/components/layout/Container.vue";
+
+export default defineComponent({
+  components: {
+    Container,
+  },
+});
+</script>
