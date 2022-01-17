2f811d25620c5161ba297b099494f32cc70cb817
take-cantik <take.cantik17@gmail.com>
2022-01-17 21:17:18 +0900

    Add: overlay

---
+++ components/shared/Overlay.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="overlay">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.overlay {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 16px;
+  padding: 44px 16px 0;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: rgba($color: #000, $alpha: 0.5);
+}
+</style>
