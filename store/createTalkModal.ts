e40e37feda41d586276c60da95e7b0e52c2e52f1
take-cantik <take.cantik17@gmail.com>
2022-01-18 18:24:18 +0900

    Delete: getter

--- store/createTalkModal.ts
+++ store/createTalkModal.ts
@@ -8,9 +8,6 @@ export const useCreateTalkModalStore = defineStore("createTalkModal", {
   state: (): State => ({
     state: false,
   }),
-  getters: {
-    state: (state) => state.state,
-  },
   actions: {
     changeState(newState: boolean) {
       this.state = newState;
--- store/editTalkModal.ts
+++ store/editTalkModal.ts
@@ -8,9 +8,6 @@ export const useEditTalkModalStore = defineStore("createEditModal", {
   state: (): State => ({
     state: false,
   }),
-  getters: {
-    state: (state) => state.state,
-  },
   actions: {
     changeState(newState: boolean) {
       this.state = newState;
