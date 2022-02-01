bc4397ca8deb5d5a0f45f4dc2e4bee00ec310240
take-cantik <take.cantik17@gmail.com>
2022-02-02 00:13:31 +0900

    Add: firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: "~/plugins/firebase.js" },
     { src: "~/plugins/pinia.js" },
     { src: "~/plugins/api/authApi.inject.ts" },
     { src: "~/plugins/api/suggestApi.inject.ts" },
